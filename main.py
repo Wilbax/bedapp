ef1bbf04561bcacb1c298b5471f76b528313f3ee
janith <janithravi7@gmail.com>
2023-01-21 17:54:09 +0530

    1.1

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
 
+
+class MainScreen(Screen):
+    pass
+
+
+class SecondScreen(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("bedx.kv")
+
+
+class bedApp(App):
+    def build(self):
+        return kv
+
+
+if __name__ == "__main__":
+    bedApp().run()
